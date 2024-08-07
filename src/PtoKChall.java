700d6c4e11d6a38e3e0628f42a2d1b9352377fe1
hernabra000 <hernabra000@warren.k12.in.us>
2024-08-07 13:27:46 -0400

    converted lbs to kg through * and printed it out.

---
+++ src/PtoKChall.java
@@ -0,0 +1,8 @@
+public class PtoKChall {
+    public static void main(String[] args) {
+        double pounds = 200;
+        double kilograms = pounds * .45359237;
+        System.out.println(pounds + "pounds is equal to " + kilograms + " kilograms.");
+    }
+}
+
